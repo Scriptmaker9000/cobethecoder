d88a8d36edb7d46a523d91a3462add8dfb0690c5
Scriptmaker9000 <111469986+Scriptmaker9000@users.noreply.github.com>
2024-07-19 17:07:34 +0300

    Add files via upload

---
+++ cobe the coder/js.js
@@ -0,0 +1,6 @@
+ function changetext() {
+var text = document.getElementsByClassName("minip")[0] 
+var texticon = document.getElementsByClassName("fa-hand-point-up")[0].className="far fa-hand-point-down"
+
+text.innerHTML="Scroll down for more" 
+ }
